bcfefe698cfd5299ead22d3b99fab610674e3975
lkanyi <kanyileah46@gmail.com>
2022-03-15 23:51:16 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,21 @@
-/*
- * File: 2-print_alphabet_x10.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * print_alphabet_x10 - Functions prints the alphabet in lowercase
+ *
  */
+
 void print_alphabet_x10(void)
 {
-	int count = 0;
 	char letter;
+	int counter;
 
-	while (count++ <= 9)
+	for (counter = 1; counter <= 10;  counter++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
+		}
 		_putchar('\n');
 	}
 }
