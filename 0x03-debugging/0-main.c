15b2798806b483dbcc3fd7763a6ab66132f6e5b9
lkanyi <kanyileah46@gmail.com>
2022-03-16 06:49:35 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - main
+* return: 0
+*/
+int main (void)
+{
+int i;
+i = 0;
+
+positive_ot_negative (i);
+
+return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+learning debugging methods, how to debug manually and how to read the error message
