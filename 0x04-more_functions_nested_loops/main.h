ee6a14f3a80c692324d4260e90c3c566cbc06601
lkanyi <kanyileah46@gmail.com>
2022-03-18 02:53:05 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,7 @@
-int _putchar (char c);
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -9,3 +12,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif
