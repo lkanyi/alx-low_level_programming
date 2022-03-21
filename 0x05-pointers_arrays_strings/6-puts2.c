d6d8455b98f99787b92f739ed39a5f30e4006920
lkanyi <kanyileah46@gmail.com>
2022-03-22 01:53:19 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * File: puts2.c- print alternating chars of string
- * @str: string
- * Return: void
- * */
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means it is correct
+ */
 
 void puts2(char *str)
 {
-	int i;
+int a;
 
-	i=0;
-	while (str[i] != '\0')
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-		i++;
-	}
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
 
-	_putchar(' n\');
-		 ;
-		 }
+_putchar ('\n');
+}
