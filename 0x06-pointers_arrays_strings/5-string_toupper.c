5e208542c56ca2af38f1c0549753aa3b50278154
lkanyi <kanyileah46@gmail.com>
2022-03-24 09:39:27 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *s)
 {
 	int count = 0;
 
-	for (*(s + count) != '\0')
+	while (*(s + count) != '\0')
 	{
 		if ((*(s + count) >= 97) && (*(s + count) <= 122))
 			*(s + count) = *(s + count) - 32;
