69e7d46895ef6a62b64fc8f3f4b79eb0d4cdc028
lkanyi <kanyileah46@gmail.com>
2022-04-06 00:55:55 +0300

    dynamic allocation, malloc and free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-
-/**create arrays-an array of chars
- * @siz:size of the array
- * @c:storage char
+/**
+ * create_array - creates an array of chars.
+ * @size: size of the array.
+ * @c: storaged char
  *
- * return pointer of arryof char
+ * Return: pointer of an array of chars
  */
 char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
 
-	if (size==0)
-		return (NULL)
-			
-			cr = malloc(sizeof(c) * size);
-	if (cr == NULL)
-		return (NULL)
+	if (size == 0)
+		return (NULL);
 
-			for (i = 0; i < size; i++)
-				cr[i] = c;
+	cr = malloc(sizeof(c) * size);
+
+	if (cr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		cr[i] = c;
+
 	return (cr);
 }
-
-
