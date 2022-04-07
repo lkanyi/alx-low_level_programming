b353659729cbfdb5587b42a432e7a2310f6501d4
lkanyi <kanyileah46@gmail.com>
2022-04-07 14:09:10 -0700

    malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked -array for prints a string
+ * @b: number of memory
+ * malloc
+ * NULL
+ * exit 
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		
+		exit(98);
+	}
+	return (p);
+}
+
