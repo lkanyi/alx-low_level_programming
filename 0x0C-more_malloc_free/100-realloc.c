9932888b802ee3b78d5a83b0dcf3708b5b796941
lkanyi <kanyileah46@gmail.com>
2022-04-08 23:26:49 +0300

    malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,33 +1,50 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _realloc -  reallocates a memory block using malloc and free
- * @ptr: pointer
- * @old_size: old size
- * @new_size: new size
- * Return: pointer
+ * *_realloc - reallocates a memory block using
+ * malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated space for ptr
+ * @new_size: new size in the byte of the new memory block
+ * Return: pointer to the newly allocated memory block
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *clone, *relloc;
-	unsigned int i;
+	char *a, *b;
+	unsigned int c;
 
-	if (ptr != NULL)
-	clone = ptr;
-	else
-	{ return (malloc(new_size)); }
 	if (new_size == old_size)
-	return (ptr);
+		return (ptr);
+
 	if (new_size == 0 && ptr != NULL)
-	{ free(ptr);
-	return (0); }
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-	return (0);
-	for (i = 0; i < (old_size || i < new_size); i++)
 	{
-		*(relloc + i) = clone[i];
+		free(ptr);
+		return (NULL);
 	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	a = malloc(new_size);
+	if (a == NULL)
+		return (NULL);
+
+	b = ptr;
+
+	if (new_size < old_size)
+	{
+		for (c = 0; c < new_size; c++)
+			a[c] = b[c];
+	}
+
+	if (new_size > old_size)
+	{
+		for (c = 0; c < old_size; c++)
+			a[c] = b[c];
+	}
+
 	free(ptr);
-return (relloc);
+	return (a);
 }
